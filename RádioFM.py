2731c74f7f7961c64c2d3d14446dd23f64d25526
Kaleo-Abreu <kaleo.abreu.2009@gmail.com>
2026-03-28 12:02:33 -0300

    Update RádioFM.py

--- RádioFM.py
+++ RádioFM.py
@@ -1,91 +1,111 @@
-estacoes = {89.5: "Cocais", 91.5: "Mix", 94.1: "Boa", 99.1: "Clube"}
+estacoes = {86.5:'Cocais', 91.5: 'Mix', 94.1:'Boa', 99.1:'Clube'}
+
 class RadioFM:
-    def __init__(self, vol_max ,estacoes):
-        self.__volume_min=0
-        self.__volume_max=vol_max
-        self.__freq_min=88
-        self.__freq_max=108
-        self.__estacoes=estacoes
-        self.__volume=None
-        self.__ligado=False
-        self.__estacao_atual=None
-        self.__frequencia_atual=None
-        self.__antena_habilitada=False
+    def __init__(self, vol_max, estacoes):
+        self.__volume_min = 0
+        self.__volume_max = vol_max
+        self.__freq_min = 88
+        self.__freq_max = 108
+        self.__estacoes = estacoes
+        self.__volume = None
+        self.__ligado = False
+        self.__estacao_atual = None
+        self.__frequencia_atual = None
+        self.__antena_habilitada = False
 
-
-
+    # ---------------- PROPRIEDADES ----------------
     @property
-    def ligar(self):
+    def ligado(self):
         return self.__ligado
 
-    @ligar.setter
-    def ligar(self):
-        if self.__ligado == False:
-            self.__ligado = True
-            self.__volume = self.__volume_min
-        else:
-            return "O rádio já está ligado"
-        
-
-    def desligar(self):
-        if self.__ligado == True:
-            self.__ligado = False
-        else:
-            return "O rádio já está desligado"
-        
     @property
     def volume(self):
         return self.__volume
-    
 
-    @volume.setter
-    def aumentar_volume(self, valor=1):
-        if valor != 1: 
-            if self.__ligado == True and valor <= self.__volume_max:
-                self.__volume = valor
-            else:
-                return "Rádio está desligado ou volume é muito alto!"
-
-        else:
-            if self.__ligado == True:
-                self.__volume += valor
-
-        
-    
-    def diminuir_volume(self, valor=-1):
-        if valor != -1: 
-            if self.__ligado == True and valor <= self.__volume_min:
-                self.__volume = valor
-            else:
-                return "Rádio está desligado ou volume é muito baixo!"
-
-        else:
-            if self.__ligado == True:
-                self.__volume =- valor
-
-         
     @property
-    def frequencia(self):
-        return self.__frequencia_atual
-    
+    def estacao_atual(self):
+        return self.__estacao_atual
 
-    @frequencia.setter
-    def mudar_frequencia(self, valor):
-        if self.__ligado == True and self.__antena_habilitada == True:
-            if valor >= self.__freq_min and valor <= self.__freq_max:
-                if valor in self.__estacoes:
-                    self.__frequencia_atual = valor
-                else:
-                    return "Frequencia não existe"
-                
-            else:
-                return "Está fora do valor minimo e máximo"
+    @property
+    def frequencia_atual(self):
+        return self.__frequencia_atual
+
+    # ---------------- LIGAR/DESLIGAR ----------------
+    def ligar(self):
+        if not self.__ligado:
+            if not self.__antena_habilitada:
+                return "A antena não está habilitada!"
             
+            self.__ligado = True
+            self.__volume = self.__volume_min
+
+            # define primeira estação
+            primeira_freq = list(self.__estacoes.keys())[0]
+            self.__frequencia_atual = primeira_freq
+            self.__estacao_atual = self.__estacoes[primeira_freq]
         else:
+            return "O rádio já está ligado"
+
+    def desligar(self):
+        if self.__ligado:
+            self.__ligado = False
+            self.__volume = None
+            self.__frequencia_atual = None
+            self.__estacao_atual = None
+        else:
+            return "O rádio já está desligado!"
+
+    # ---------------- ANTENA ----------------
+    def habilitar_antena(self):
+        self.__antena_habilitada = True
+
+    def desabilitar_antena(self):
+        self.__antena_habilitada = False
+
+    # ---------------- VOLUME ----------------
+    @volume.setter
+    def volume(self, valor):
+        if not self.__ligado:
+            return "Rádio está desligado!"
+
+        if self.__volume_min <= valor <= self.__volume_max:
+            self.__volume = valor
+        else:
+            return "Volume fora do limite!"
+
+    def aumentar_volume(self, passo=1):
+        if not self.__ligado:
+            return "Rádio está desligado!"
+
+        novo_volume = self.__volume + passo
+        self.__volume = min(novo_volume, self.__volume_max)
+
+    def diminuir_volume(self, passo=1):
+        if not self.__ligado:
+            return "Rádio está desligado!"
+
+        novo_volume = self.__volume - passo
+        self.__volume = max(novo_volume, self.__volume_min)
+
+    # ---------------- FREQUÊNCIA ----------------
+    @frequencia_atual.setter
+    def frequencia_atual(self, valor):
+        if not (self.__ligado and self.__antena_habilitada):
             return "O rádio está desligado ou a antena está desabilitada!"
 
+        if valor in self.__estacoes:
+            self.__frequencia_atual = valor
+            self.__estacao_atual = self.__estacoes[valor]
+        else:
+            return "Estação inexistente"
 
+    def proxima_estacao(self):
+        if not (self.__ligado and self.__antena_habilitada):
+            return "O rádio está desligado ou a antena está desabilitada!"
 
-                
+        chaves = list(self.__estacoes.keys())
+        idx = chaves.index(self.__frequencia_atual)
+        proximo_idx = (idx + 1) % len(chaves)
 
-
+        self.__frequencia_atual = chaves[proximo_idx]
+        self.__estacao_atual = self.__estacoes[self.__frequencia_atual]
